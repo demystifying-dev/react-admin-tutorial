a01276bcde3c9a40e7f8ec2af2f81d93d49e8e24
victorkane <victorkane@gmail.com>
2019-03-21 20:42:06 +0000

    Added Menu Icons

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-// Added Search And Filters To The List
+// Added Menu Icons
 // https://marmelab.com/react-admin/Tutorial.html
 
 import React from 'react';
@@ -6,13 +6,15 @@ import { Admin, Resource } from 'react-admin';
 import { PostList, PostEdit, PostCreate } from './posts'
 import { UserList } from './users'
 import jsonServerProvider from 'ra-data-json-server';
+import PostIcon from '@material-ui/icons/Book'
+import UserIcon from '@material-ui/icons/Group'
 
 const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
 
 const App = () => (
   <Admin dataProvider={dataProvider} >
-    <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} />
-    <Resource name="users" list={UserList} />
+    <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon} />
+    <Resource name="users" list={UserList} icon={UserIcon} />
   </Admin>
 )
 
