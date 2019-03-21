475fdfa9b6a609b6c8a76344723cae34c9b57cee
victorkane <victorkane@gmail.com>
2019-03-21 09:43:02 +0000

    Mapping API Endpoints With Resources

--- src/App.js
+++ src/App.js
@@ -1,43 +1,17 @@
-// Making Contact With The API Using a Data Provide
+// Mapping API Endpoints With Resources
 // https://marmelab.com/react-admin/Tutorial.html
 //
 
-/*
-$ curl http://jsonplaceholder.typicode.com/users/2
-{
-  "id": 2,
-  "name": "Ervin Howell",
-  "username": "Antonette",
-  "email": "Shanna@melissa.tv",
-  "address": {
-    "street": "Victor Plains",
-    "suite": "Suite 879",
-    "city": "Wisokyburgh",
-    "zipcode": "90566-7771",
-    "geo": {
-      "lat": "-43.9509",
-      "lng": "-34.4618"
-    }
-  },
-  "phone": "010-692-6593 x09125",
-  "website": "anastasia.net",
-  "company": {
-    "name": "Deckow-Crist",
-    "catchPhrase": "Proactive didactic contingency",
-    "bs": "synergize scalable supply-chains"
-  }
-}
-*/
-
-// Browser reads:
-//    React-admin is properly configured.
-//    Now you can add a first <Resource> as child of <Admin>.
-
 import React from 'react';
-import { Admin, Resource } from 'react-admin';
+import { Admin, Resource, ListGuesser } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
 
 const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
-const App = () => <Admin dataProvider={dataProvider} />;
+
+const App = () => (
+  <Admin dataProvider={dataProvider} >
+    <Resource name="users" list={ListGuesser} />
+  </Admin>
+)
 
 export default App;
