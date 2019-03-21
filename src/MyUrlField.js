72823e6846afa608ff3c0fb21b51bc3337e30c02
victorkane <victorkane@gmail.com>
2019-03-21 12:41:07 +0000

    Ensure that className is properly provided to LaunchIcon

--- src/MyUrlField.js
+++ src/MyUrlField.js
@@ -12,12 +12,12 @@ const styles = {
     },
 }
 
-const MyUrlField = ({ record = {}, source }) => {
+const MyUrlField = ({ record = {}, source }, classes) => {
   // use http:// so current window path is not prefixed in link, see https://stackoverflow.com/questions/38573892/i-try-to-make-my-material-ui-raisedbutton-link-to-an-external-url-without-succes
   return(
     <a href={`http://${record[source]}`} >
       {record[source]}
-      <LaunchIcon className={styles.icon} />
+      <LaunchIcon className={classes.icon} />
     </a>
   )
 }
