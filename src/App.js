e7c42bbbe9e04f1395d8749b656a6613f771cbaf
victorkane <victorkane@gmail.com>
2019-03-21 09:48:39 +0000

    This code guesses an appropriate Datagrid for the Resource, and writes it to console

--- src/App.js
+++ src/App.js
@@ -2,6 +2,29 @@
 // https://marmelab.com/react-admin/Tutorial.html
 //
 
+/*
+
+This code guesses an appropriate Datagrid for the Resource, and writes it to console:
+
+Guessed List: 
+
+export const UserList = props => (
+  <List {...props}>
+    <Datagrid rowClick="edit">
+      <TextField source="id" />
+      <TextField source="name" />
+      <TextField source="username" />
+      <EmailField source="email" />
+      <TextField source="address.street" />
+      <TextField source="phone" />
+      <TextField source="website" />
+      <TextField source="company.name" />
+    </Datagrid>
+  </List> );
+);
+
+*/
+
 import React from 'react';
 import { Admin, Resource, ListGuesser } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
