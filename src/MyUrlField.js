1b06599a04b0f72c57e5d473cda6d173e93846dd
victorkane <victorkane@gmail.com>
2019-03-21 11:28:43 +0000

    Added interpolation to prefix http:// so that current window path is not prefixed to external url for website column

--- src/MyUrlField.js
+++ src/MyUrlField.js
@@ -1,8 +1,12 @@
 import React from 'react';
 
-const MyUrlField = ({ record = {}, source }) =>
-      <a href={record[source]}>
-            {record[source]}
-    </a>;
+const MyUrlField = ({ record = {}, source }) => {
+  // use http:// so current window path is not prefixed in link, see https://stackoverflow.com/questions/38573892/i-try-to-make-my-material-ui-raisedbutton-link-to-an-external-url-without-succes
+  return(
+    <a href={`http://${record[source]}`} >
+      {record[source]}
+    </a>
+  )
+}
 
 export default MyUrlField;
