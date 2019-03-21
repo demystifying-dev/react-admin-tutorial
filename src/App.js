76db507601fc102e5911ca893bd8396ecfcf8edc
victorkane <victorkane@gmail.com>
2019-03-21 13:50:07 +0000

    As a first step in making the Edit button functional, use the EditGuesser with the PostList component

--- src/App.js
+++ src/App.js
@@ -1,44 +1,27 @@
-// Complete the PostList component with further customizations, including an Edit button
+// As a first step in making the Edit button functional, use the EditGuesser with the PostList component 
 // https://marmelab.com/react-admin/Tutorial.html
 
 /*
 
-Guessed List for posts from the posts API endpoint:
+PostList EditGuesser recommendations:
 
-Guessed List:
+Guessed Edit:
 
-export const PostList = props => (
-  <List {...props}>
-    <Datagrid rowClick="edit">
-      <ReferenceField source="userId" reference="users"><TextField source="id" /></ReferenceField>
-      <TextField source="id" />
-      <TextField source="title" />
-      <TextField source="body" />
-    </Datagrid>
-  </List>
-);
-
-Guessed List for users from earlier commit (now imported from our own ./users.js). Browser shows same list of users: 
-
-export const UserList = props => (
-  <List {...props}>
-    <Datagrid rowClick="edit">
-      <TextField source="id" />
-      <TextField source="name" />
-      <TextField source="username" />
-      <EmailField source="email" />
-      <TextField source="address.street" />
-      <TextField source="phone" />
-      <TextField source="website" />
-      <TextField source="company.name" />
-    </Datagrid>
-  </List>
-);
+export const PostEdit = props => (
+    <Edit {...props}>
+        <SimpleForm>
+            <ReferenceInput source="userId" reference="users"><SelectInput optionText="id" /></ReferenceInput>
+            <TextInput source="id" />
+            <TextInput source="title" />
+            <TextInput source="body" />
+        </SimpleForm>
+    </Edit>
+)
 
 */
 
 import React from 'react';
-import { Admin, Resource } from 'react-admin';
+import { Admin, Resource, EditGuesser } from 'react-admin';
 import { PostList } from './posts'
 import { UserList } from './users'
 import jsonServerProvider from 'ra-data-json-server';
@@ -47,7 +30,7 @@ const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
 
 const App = () => (
   <Admin dataProvider={dataProvider} >
-    <Resource name="posts" list={PostList} />
+    <Resource name="posts" list={PostList} edit={EditGuesser} />
     <Resource name="users" list={UserList} />
   </Admin>
 )
