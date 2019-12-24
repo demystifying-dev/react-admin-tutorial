a4aa2e9b8b2c9f58a4914efa9d1ed9cb5da51f2b
Victor Kane <victorkane@gmail.com>
2019-12-24 14:40:30 -0500

    Customizing the Menu Icons
    Customize Posts and Users menu item icons in sidebar with off-the-shelf material-ui icons included in react-admin

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { Admin, Resource } from 'react-admin';
 import { PostList, PostEdit, PostCreate } from './posts';
 import { UserList } from './users';
 import jsonServerProvider from 'ra-data-json-server';
+import PostIcon from '@material-ui/icons/Book';
+import UserIcon from '@material-ui/icons/Group';
 
 const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
 const App = () => (
     <Admin dataProvider={dataProvider}>
-       <Resource name="users" list={UserList} />
-       <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} />
+       <Resource name="users" list={UserList} icon={UserIcon} />
+       <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon} />
     </Admin>
 );
 
