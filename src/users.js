a3dda4ed9cc6b0b8cea0dc1ffa5f285db93bb745
Victor Kane <victorkane@gmail.com>
2019-12-24 08:03:43 -0500

    Using field types
    Website items now clickable by using UrlField

--- src/users.js
+++ src/users.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { List, Datagrid, TextField, EmailField } from 'react-admin';
+import { List, Datagrid, TextField, EmailField, UrlField } from 'react-admin';
 
 export const UserList = props => (
     <List {...props}>
@@ -8,7 +8,7 @@ export const UserList = props => (
             <TextField source="name" />
             <EmailField source="email" />
             <TextField source="phone" />
-            <TextField source="website" />
+            <UrlField source="website" />
             <TextField source="company.name" />
         </Datagrid>
     </List>
