8648ae671addcac3b5e32aa03e1887f3fe1cf409
Victor Kane <victorkane@gmail.com>
2019-12-24 12:23:51 -0500

    Handling Relationships
    Adding posts as resource and grabbing its schema with ListGuesser

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import React from 'react';
-import { Admin, Resource } from 'react-admin';
+import { Admin, Resource, ListGuesser } from 'react-admin';
 import { UserList } from './users';
 import jsonServerProvider from 'ra-data-json-server';
+
+/*
+Guessed List:
+
+export const PostList = props => (
+    <List {...props}>
+        <Datagrid rowClick="edit">
+            <ReferenceField source="userId" reference="users"><TextField source="id" /></ReferenceField>
+            <TextField source="id" />
+            <TextField source="title" />
+            <TextField source="body" />
+        </Datagrid>
+    </List>
+);
+*/
 
 const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
 const App = () => (
     <Admin dataProvider={dataProvider}>
        <Resource name="users" list={UserList} />
+       <Resource name="posts" list={ListGuesser} />
     </Admin>
 );
 
