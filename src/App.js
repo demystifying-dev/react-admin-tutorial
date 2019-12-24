f860649a16ab482625b9434c2e9a346a72427e15
Victor Kane <victorkane@gmail.com>
2019-12-24 13:03:17 -0500

    Adding Creation and Editing Capabilities
    Using <EditGuesser> to bootstrap the react-admin <Edit> component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Admin, Resource } from 'react-admin';
+import { Admin, Resource, EditGuesser } from 'react-admin';
 import { PostList } from './posts';
 import { UserList } from './users';
 import jsonServerProvider from 'ra-data-json-server';
@@ -8,7 +8,7 @@ const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
 const App = () => (
     <Admin dataProvider={dataProvider}>
        <Resource name="users" list={UserList} />
-       <Resource name="posts" list={PostList} />
+       <Resource name="posts" list={PostList} edit={EditGuesser} />
     </Admin>
 );
 
