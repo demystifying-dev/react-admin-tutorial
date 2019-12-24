9d3221915a6b26527777f68ab4e37ad1d9118145
Victor Kane <victorkane@gmail.com>
2019-12-24 07:31:54 -0500

    Mapping API Endpoints With Resources
    The magic of Resource and ListGuesser

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
 import React from 'react';
-import { Admin } from 'react-admin';
+import { Admin, Resource, ListGuesser } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
 
+/*
+ListGuesser shows the following in the browser console:
+
+Guessed List:
+
+export const UserList = props => (
+    <List {...props}>
+        <Datagrid rowClick="edit">
+            <TextField source="id" />
+            <TextField source="name" />
+            <TextField source="username" />
+            <EmailField source="email" />
+            <TextField source="address.street" />
+            <TextField source="phone" />
+            <TextField source="website" />
+            <TextField source="company.name" />
+        </Datagrid>
+    </List>
+);
+*/
+
 const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
-const App = () => <Admin dataProvider={dataProvider} />;
+const App = () => (
+  <Admin dataProvider={dataProvider}>
+    <Resource name="users" list={ListGuesser} />
+  </Admin>
+);
 
 export default App;
