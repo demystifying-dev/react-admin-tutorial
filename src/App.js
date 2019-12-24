e3c6f6354902dcb17ae75f065c0e7d255cc58d52
Victor Kane <victorkane@gmail.com>
2019-12-24 13:07:24 -0500

    Adding Creation and Editing Capabilities
    Output from EditGuesser

--- src/App.js
+++ src/App.js
@@ -4,6 +4,21 @@ import { PostList } from './posts';
 import { UserList } from './users';
 import jsonServerProvider from 'ra-data-json-server';
 
+/*
+Guessed Edit:
+
+export const PostEdit = props => (
+    <Edit {...props}>
+        <SimpleForm>
+            <ReferenceInput source="userId" reference="users"><SelectInput optionText="id" /></ReferenceInput>
+            <TextInput source="id" />
+            <TextInput source="title" />
+            <TextInput source="body" />
+        </SimpleForm>
+    </Edit>
+);
+*/
+
 const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
 const App = () => (
     <Admin dataProvider={dataProvider}>
