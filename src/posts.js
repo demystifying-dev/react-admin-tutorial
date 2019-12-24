e0578a0e9c41b1345c636e0b4c16eadacc09a4e4
Victor Kane <victorkane@gmail.com>
2019-12-24 13:14:47 -0500

    Adding Creation and Editing Capabilities
    Add new <PostEdit> component based on output from the <EditGuesser> component

--- src/posts.js
+++ src/posts.js
@@ -1,5 +1,15 @@
 import React from 'react';
-import { List, Datagrid, TextField, ReferenceField, EditButton } from 'react-admin';
+import { List,
+         Datagrid,
+         TextField,
+         ReferenceField,
+         EditButton,
+         Edit,
+         SimpleForm,
+         ReferenceInput,
+         SelectInput,
+         TextInput
+       } from 'react-admin';
 
 export const PostList = props => (
     <List {...props}>
@@ -12,4 +22,15 @@ export const PostList = props => (
             <EditButton />
         </Datagrid>
     </List>
+);
+
+export const PostEdit = props => (
+    <Edit {...props}>
+        <SimpleForm>
+            <ReferenceInput source="userId" reference="users"><SelectInput optionText="id" /></ReferenceInput>
+            <TextInput source="id" />
+            <TextInput source="title" />
+            <TextInput source="body" />
+        </SimpleForm>
+    </Edit>
 );
