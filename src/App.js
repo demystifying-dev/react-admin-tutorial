a31f65bbc09678c73530fedfa3ef988b1d8b53f3
Victor Kane <victorkane@gmail.com>
2019-12-24 12:29:03 -0500

    Handling Relationships
    Creating the PostList component based on the code dumped by the guesser, including the ReferenceField

--- src/App.js
+++ src/App.js
@@ -1,28 +1,14 @@
 import React from 'react';
-import { Admin, Resource, ListGuesser } from 'react-admin';
+import { Admin, Resource } from 'react-admin';
+import { PostList } from './posts';
 import { UserList } from './users';
 import jsonServerProvider from 'ra-data-json-server';
-
-/*
-Guessed List:
-
-export const PostList = props => (
-    <List {...props}>
-        <Datagrid rowClick="edit">
-            <ReferenceField source="userId" reference="users"><TextField source="id" /></ReferenceField>
-            <TextField source="id" />
-            <TextField source="title" />
-            <TextField source="body" />
-        </Datagrid>
-    </List>
-);
-*/
 
 const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
 const App = () => (
     <Admin dataProvider={dataProvider}>
        <Resource name="users" list={UserList} />
-       <Resource name="posts" list={ListGuesser} />
+       <Resource name="posts" list={PostList} />
     </Admin>
 );
 
