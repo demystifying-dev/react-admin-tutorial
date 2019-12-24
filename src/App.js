65b75bf8339a57ee0380f0da1c133d5885b81e00
Victor Kane <victorkane@gmail.com>
2019-12-24 15:03:31 -0500

    Using a Custom Home Page
    Add our own Dashboard to Admin pages as a dashboard prop

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Admin, Resource } from 'react-admin';
+import Dashboard from './Dashboard';
 import { PostList, PostEdit, PostCreate } from './posts';
 import { UserList } from './users';
 import jsonServerProvider from 'ra-data-json-server';
@@ -8,7 +9,7 @@ import UserIcon from '@material-ui/icons/Group';
 
 const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
 const App = () => (
-    <Admin dataProvider={dataProvider}>
+    <Admin dashboard={Dashboard} dataProvider={dataProvider}>
        <Resource name="users" list={UserList} icon={UserIcon} />
        <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon} />
     </Admin>
